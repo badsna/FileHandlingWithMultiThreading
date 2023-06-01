2daf3c965ef11e33032ad33e173ad5573597607e
badsna <badsnastha@gmail.com>
2023-06-01 17:31:51 +0545

    file handling with multithreading remaning threadpool

---
+++ src/main/java/org/example/FileHandlingWithMultiThreading/Curr_altitude_fit.java
@@ -0,0 +1,16 @@
+package org.example.FileHandlingWithMultiThreading;
+
+import java.util.Comparator;
+ class Curr_altitude_fit implements Comparator<FlightInfo> {
+    @Override
+    public int compare(FlightInfo o1, FlightInfo o2) {
+        int aa = o1.getCurr_altitude_fit();
+        int bb = o2.getCurr_altitude_fit();
+        if (aa < bb) {
+            return -1;
+        } else if (aa > bb) {
+            return 1;
+        }
+        return 0;
+    }
+}
